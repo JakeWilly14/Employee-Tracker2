2274971a79deedf0df8b1d321c8cd7a59089b6ec
Jakewilly14 <jakewilly14@gmail.com>
2023-12-12 22:25:59 -0600

    Version 1.0 Commit

--- db/seeds.sql
+++ db/seeds.sql
@@ -163,20 +163,3 @@ VALUES (
   ),
   NULL
 );
-
--- Store manager_id in temporary variable
--- SET @John_id = (
---     SELECT id
---     FROM employee
---     WHERE first_name = "John"
---         AND last_name = "Doe"
--- );
-
--- UPDATE employee
--- SET manager_id = @John_id
--- WHERE first_name = "Mike"
---     AND last_name = "Chan";​
-
--- SELECT *
--- FROM employee
--- WHERE manager_id IS NOT NULL;
